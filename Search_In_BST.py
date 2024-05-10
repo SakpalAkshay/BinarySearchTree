f6fc955a89ea30ac063b049752a1555ef4478e3e
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-05-09 19:53:24 -0700

    Pushed code to search in BST

--- Search_In_BST.py
+++ Search_In_BST.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        while root is not None:
+            if root.val == val:
+                return True
+            elif root.val > val:
+                root = root.left
+            else:
+                root = root.right
+        return False
